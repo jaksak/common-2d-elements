539d31120196cc034f2bdf7d5431e3cf4bf9ce2e
jaksak <jkrzemien28@gmail.com>
2020-10-29 14:11:05 +0100

    SpecialBehaviourPositionsFactory: collision != no move

--- src/main/java/pl/longhorn/common/game/elements/SpecialBehaviourPositionsFactory.java
+++ src/main/java/pl/longhorn/common/game/elements/SpecialBehaviourPositionsFactory.java
@@ -10,13 +10,22 @@ import java.util.Objects;
 
 public class SpecialBehaviourPositionsFactory {
 
-    public static SpecialBehaviourPositions create(String collisions, Map<Position, SpecialMapBehaviour> specialMapBehaviours) {
+    public static SpecialBehaviourPositions createCollisionAsNoMove(String collisions, Map<Position, SpecialMapBehaviour> specialMapBehaviours){
         SpecialMapBehaviour collision = SpecialMapBehaviour.collision();
+        return create(collisions, specialMapBehaviours, collision);
+    }
+
+    public static SpecialBehaviourPositions createCollisionAsHardMove(String collisions, Map<Position, SpecialMapBehaviour> specialMapBehaviours, int price){
+        SpecialMapBehaviour collision = SpecialMapBehaviour.price(price);
+        return create(collisions, specialMapBehaviours, collision);
+    }
+
+    private static SpecialBehaviourPositions create(String collisions, Map<Position, SpecialMapBehaviour> specialMapBehaviours, SpecialMapBehaviour collisionBehaviour) {
         Arrays.stream(collisions.split(";"))
                 .filter(Objects::nonNull)
                 .filter(string -> !string.isBlank())
                 .map(SpecialBehaviourPositionsFactory::toPosition)
-                .forEach(position -> specialMapBehaviours.put(position, collision));
+                .forEach(position -> specialMapBehaviours.put(position, collisionBehaviour));
         return new SpecialBehaviourPositions(specialMapBehaviours);
     }
 
